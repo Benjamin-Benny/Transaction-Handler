eeae8ddd060f63881a0409ee4c059495bb062b90
Benjamin Benny <35957758+Benjamin-Benny@users.noreply.github.com>
2024-06-12 00:01:32 +0530

    spring starter files

---
+++ backend/src/main/java/com/project/transcaction_handler/TranscactionHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.project.transcaction_handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscactionHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscactionHandlerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/transcaction_handler/TranscactionHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.transcaction_handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscactionHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
